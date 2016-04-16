077a129345465a85568522009090231cffb94611
Juhwan Jeong <j7jeong@uwaterloo.ca>
2016-04-16 07:25:33 -0400

    Initial commit of DogePDF

---
+++ DogePDF/DogePDF/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DogePDF
+//
+//  Created by Juhwan Jeong on 4/16/16.
+//  Copyright © 2016 David Jeong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
